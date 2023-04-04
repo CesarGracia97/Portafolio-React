4a579d2a8654054961ae212294bc76206e408b5c
CESAR GRACIA <cesar.gracia@pucese.edu.ec>
2023-04-04 14:35:31 -0500

    Agregado los elementos about y qualifications

---
+++ src/components/qualifications/qualifications.jsx
@@ -0,0 +1,45 @@
+import "./qualifications.css";
+const Qualifications = () => {
+    return (
+        <div>
+            <span className="quali-text">My Qualifications</span>
+            <div className="qualifications-container">
+                <div className="qualification">
+                    <div className="content html"></div>
+                    <h1>HTML</h1>
+                </div>
+                <div className="qualification">
+                    <div className="content css"></div>
+                    <h1>CSS</h1>
+                </div>
+                <div className="qualification">
+                    <div className="content javascript"></div>
+                    <h1>Javascript</h1>
+                </div>
+                <div className="qualification">
+                <div className="content bootstrap"></div>
+                    <h1>Bootstrap</h1>
+                </div>
+                <div className="qualification">
+                    <div className="content react"></div>
+                    <h1>React</h1>
+                </div>
+                <div className="qualification">
+                    <div className="content c-plus-plus"></div>
+                    <h1>C++</h1>
+                </div>
+                <div className="qualification">
+                    <div className="content c-sharp"></div>
+                    <h1>C#</h1>
+                </div>
+                <div className="qualification">
+                    <div className="content kotlin"></div>
+                    <h1>Kotlin</h1>
+                </div>
+            </div>
+        </div>
+    );
+    
+};
+
+export default Qualifications;
